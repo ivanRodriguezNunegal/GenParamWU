7e683536522f9fa17b8b007a95821dd951e19746
ivanrodriguezgomez <ivanrodriguezgomez@outlook.com>
2023-03-27 15:20:27 +0200

    Nuevo proyecto generacion parámetros User FCT

---
+++ Data.DBBOFCT/ExtendedEntities/Grupo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.DBBOFCT
+{
+    public partial class Grupo
+    {
+
+        /// <summary>
+        /// Obtenemos una lista de Items
+        /// </summary>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public static List<Grupo> GetList(BOFCTEntities db)
+        {
+                return (from g in db.Grupo
+                        orderby g.GrupoID
+                        select g).ToList();
+        }
+
+
+        /// <summary>
+        /// Inserción nuevo registro
+        /// </summary>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public static void Insert(BOFCTEntities db, Grupo g)
+        {
+            db.Grupo.Add(g);
+        }
+    }
+}
---
+++ GenParametroWU/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Data.DBBOFCT;
+
+namespace GenParametroWU
+{
+    public partial class Form1 : Form
+    {
+
+#region Metodos
+
+        public Form1()
+        {
+            InitializeComponent();
+            using (BOFCTEntities db = new BOFCTEntities())
+            {
+                LoadControls(db);
+            }
+        }
+
+        private void LoadControls(BOFCTEntities db)
+        {
+            List<Grupo> listGrupos = new List<Grupo>();
+            listGrupos = Grupo.GetList(db);
+
+            cbGrupos.DataSource = listGrupos;
+            cbGrupos.DisplayMember = "Nombre";
+            cbGrupos.ValueMember = "GrupoID";
+        }
+
+        #endregion
+
+
+        #region Eventos
+
+        #endregion
+
+    }
+}
